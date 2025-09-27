05abd7e7fb6f39a8ae7ab3c9592e1d04a95be0c6
Sumit Dhawde <sumitdhawade555@gmail.com>
2025-09-27 23:29:06 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,260 @@
+import internshiplogo from "../images/internship.png";
+import itlogo from "../images/it.png"
+import coffeelogo from "../images/home1.png"
+import tasklogo from "../images/task.png"
+import ekart from "../images/ekart.png"
+import ctboralogo from "../images/ctbora.png"
+import sinhgadlogo from "../images/sinhgad.png"
+export const Bio = {
+  name: "Sumit Dhawade",
+  roles: [
+    "Web Developer",
+    "Fronend Developer",
+    "BackEnd Developer",
+    "FullStack Developer",
+    "Programmer",
+  ],
+  description:
+    "Versatile developer with a strong foundation in Problem solving and eager to learning new things, complemented by expertise in Front-End Development and User Interface Design. Proficient in HTML5, CSS and JavaScript, with experience in Node.js for backend development. Skilled in multiple programming languages including Java,and Python.Adept at database management using SQL.",
+  github: "https://github.com/sumitdhawde/",
+  resume:
+    "https://drive.google.com/file/d/1PbiGrrKEDsh1ms7J9hTmb1LawTcq7d85/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/sumit-dhawade-1324a2268/",
+  
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+     {
+        name: "java",
+        image: "https://www.vectorlogo.zone/logos/java/java-icon.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      // {
+      //   name: "Postgresql",
+      //   image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      // },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      // {
+      //   name: "Firebase",
+      //   image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      // },
+      
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      // {
+      //   name: "Postman",
+      //   image: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      // },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: internshiplogo,
+    role: "junior developer ",
+    company: "internship studio Pvt. Ltd.",
+    date: "jul 2024 - jan 2025",
+    desc: "Working with integrating Machine Learning libraries into Java based automation testing tool",
+    skills: [
+      "Python",
+      "machine learning",
+
+     
+    ],
+    doc: "https://github.com/SumitDhawde",
+  },
+  {
+    id: 0,
+    img:itlogo,
+    role: "web developer Intern",
+    company: "Technobij Pvt. Ltd.",
+    date: "Aug 2022 - Sep 2022",
+    desc: "Completed a training program with an A grade,focusing on technologies and projects , web development,database management.",
+    skills: [
+      
+      "Web developments",
+      "SDLC",
+      "Database",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://sumitdhawde.github.io/coffee_shop_UI/",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: sinhgadlogo,
+    school: "Sinhgad Institute of Technology, Lonavala",
+    date: "Aug 2022 - Jun 2025",
+    grade: "7.18 CGPA",
+    desc: "I am recent graduated with Bachelor's degree in Computer Science  at Sinhgad Institute of Technology, Lonavala. I have completed 8 semesters and have a CGPA of 7.18. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Litereray Student Club  at SIT.",
+    degree: "Bachelor of Engineering - BE, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: ctboralogo,
+    school: "CTBora College shirur",
+    date: "Aug 2020 - Jun 2021",
+    grade: "79.67%",
+    desc: "I completed my HSC from CTBora college ,Shirur, where I studied  12'th in science  and acored 79.67%.",
+    degree: "HSC",
+  },
+  
+];
+
+export const projects = [
+  // {
+  //   id: 9,
+  //   title: "CropShield",
+  //   date: "Sep 2024 - May 2025",
+  //   description:
+  //     " Developed a real-time crop disease detection and monitoring mobile appplication. The system accurately identified diseases of crops with accuracy",
+  //   image:
+  //     CropShield,
+  //   tags: [
+  //     "Javascript",
+  //     "React Js",
+  //     "Python",
+  //     "Machine learning",
+  //     "TensorFlow",
+  //     "Keras",
+  //     "CNN",
+  //     "Flask",
+  //   ],
+  //   category: ["machine learning", "android app"],
+  //   github: "https://github.com/mayurifegade0606/CropShieldApp",
+  //   //webapp:
+  // },
+  
+  {
+    id: 1,
+   
+    title: "Task Management System",
+    date: "march 2025 - April 2025",
+    description:
+      "A Task Management System helps users organize, track, and manage tasks efficiently. It allows users to add, view, and delete tasks",
+    image:
+      tasklogo,
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "node.js",
+     
+      "MySQL",
+    ],
+    category: "web developments",
+    github: " https://github.com/SumitDhawde/Task-Management-system",
+    
+  },
+  {
+    id: 2,
+    title: "Coffee Shop Ui",
+    date: "jan 2025 - April 2025",
+    description:
+  "Developed a responsive coffee shop website using HTML, CSS, and JavaScript to enhance the user experience.",
+    image:
+     coffeelogo,
+    tags: ["HTML", "CSS", "Javascript", "Bootstrap"],
+    category: "web developments",
+    github: " https://sumitdhawde.github.io/coffee_shop_UI/",
+    
+    
+  },
+  {
+    id: 9,
+    title: "electronic Eye for Blind using ML",
+    date: "jan 2025 - April 2025",
+    description:
+  "Built a real-time object detection and distance estimation system using YOLOv8 and OpenCV. Integrated multithreaded text-to-speech",
+    image:
+      ekart,
+    tags: ["pyttsx3", "opencv", "python", "machine learning"],
+    category: "machine learning",
+    github: "  https://github.com/SumitDhawde/electronic-eye-for-blind-using-ml-and-yelo-algorithm/",
+    
+    
+  },
+  
+  
+];
+
+export const TimeLineData = [
+  { year: 2019, text: "Started my journey" },
+  { year: 2021, text: "Completed HSC" },
+  { year: 2022, text: "Experties in web development" },
+  { year: 2024, text: "Explored Machine Learning and java" },
+  { year: 2025, text: "buiding projects" },
+];
